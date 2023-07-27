5a7c182f9c82ab14dfa16047d1cb0d4222bc200f
srihari <srihari.r@taski.in>
2023-07-27 20:09:59 +0530

    sonar1

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://43.204.217.18:9000/sonar',
+  serverUrl: 'http://43.204.217.18:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
